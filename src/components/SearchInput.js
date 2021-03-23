b766af165d426398b9d72642b5a4c2e00fe7a70b
RilDev <c.public@tutanota.com>
2021-03-23 15:56:32 +0100

    darken placeholder to fit history log date color

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -8,7 +8,7 @@ export const SearchInput = {
       :placeholder="placeholder"
       :value="value"
       @input="$emit('update:value', $event.target.value);$emit('getResults', $event)"
-      class="rounded-md border-gray-900 mt-3 px-5 py-3 shadow-sm w-full focus:outline-none"
+      class="rounded-md border-gray-900 mt-3 px-5 py-3 shadow-sm w-full focus:outline-none placeholder-gray-500"
       />
       <div @click="$emit('clearSearch', reference)" title="Clear Search" class="absolute top-6 right-4 cursor-pointer"><svg width="24" height="24" viewBox="0 0 24 24"><path d="M20 6.91L17.09 4L12 9.09L6.91 4L4 6.91L9.09 12L4 17.09L6.91 20L12 14.91L17.09 20L20 17.09L14.91 12L20 6.91Z" /></svg></div>
     </div>
