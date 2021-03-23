39d3c62785b6304bdecf62f67eddfd7b2398b554
RilDev <c.public@tutanota.com>
2021-03-23 14:51:12 +0100

    add rel=nooopener on links

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ export const Title = {
     <h1 class="text-4xl text-center text-blue-500 font-bold">API Vuer</h1>
     <div class="text-center">
       Powered by
-      <a href="https://api.publicapis.org/" target="_blank" class="text-blue-500">Public APIs</a>
+      <a href="https://api.publicapis.org/" target="_blank" rel="noopener" class="text-blue-500">Public APIs</a>
     </div>
     `,
 };
